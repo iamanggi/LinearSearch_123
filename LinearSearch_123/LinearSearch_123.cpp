e3637eac153a0787f86d9e095f04e809350d6a4b
iamanggi <anggipuspita452@gmail.com>
2023-04-13 14:36:16 +0700

    melakukan prosedur input pada while

--- LinearSearch_123/LinearSearch_123.cpp
+++ LinearSearch_123/LinearSearch_123.cpp
@@ -4,3 +4,15 @@ using namespace std;
 int arr[20];		//array to be search
 int n;				// number of element in the array 
 int i;				// index of array element 
+
+
+void input() {
+	while (true) {
+		cout << "enter the number of element in the array: ";
+		cin >> n;
+		if ((n > 0) && (n <= 20))
+			break;
+		else
+			cout << "\nArray should have minimum 1 and maximum 20 elemnts.\n\n";
+	}
+}

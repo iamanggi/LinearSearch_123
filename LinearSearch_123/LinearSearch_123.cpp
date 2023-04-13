1caf5f4cd2e3976bd77e9c29a84eaf1089f30a95
iamanggi <anggipuspita452@gmail.com>
2023-04-13 14:57:09 +0700

    melakukan langkah algoritma ke 5

--- LinearSearch_123/LinearSearch_123.cpp
+++ LinearSearch_123/LinearSearch_123.cpp
@@ -48,6 +48,11 @@ void LinearSearch()
 		for (i = 0; i < n; i++)										// langkah no 2,3 & 4 algoritma
 		{
 			ctr++;
+			if (arr[i] == item)										// langkah no 5 algoritma 
+			{
+				cout << "\n" << item << "found at position" << (i + 1) << endl;
+			}
+
 		}
 	}
 }
